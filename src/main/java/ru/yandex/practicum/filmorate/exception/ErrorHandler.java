bd2b0621b33fcbcc404625dee90adf0dbecd93cf
Kriglz <Kriglz@yandex.ru>
2024-02-06 13:01:16 +0300

    new Exception

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.controller.UserController;
 
@@ -52,6 +53,12 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleUnknownUpdateException(final UnknownUpdateException e) {
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleResponseStatusException(ResponseStatusException e) {
         return Map.of("error", e.getMessage());
     }
 }
