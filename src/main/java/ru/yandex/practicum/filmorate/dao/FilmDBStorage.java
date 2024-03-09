895cc68f8d4b9fd964f47f44d3c8df257570014c
ilya364 <tsh335nextYeAr@yandex.ru>
2024-03-09 21:21:15 +0300

    отредачить схему бд, рефакторинг методов

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDBStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDBStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.dao;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
