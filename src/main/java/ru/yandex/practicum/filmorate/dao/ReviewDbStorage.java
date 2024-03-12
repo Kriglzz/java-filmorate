a9c95d4625af1a795efa3690a4961774aee55895
Egor <alexanoffegor@yandex.ru>
2024-03-12 17:38:59 +0300

    removed unused import

--- src/main/java/ru/yandex/practicum/filmorate/dao/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/ReviewDbStorage.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.model.Review;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -5,8 +5,6 @@ import lombok.experimental.SuperBuilder;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
 
 @Data
 @NoArgsConstructor
